afaff95d8c496b439277a960c979c6e06a22ba3b
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-08-21 11:48:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function greet(time) {
 /* Write your implementation of displayMessage() */
 
 function displayMessage(message) {
-  document.getElementById.innerText
+  document.getElementById.innerText = message
   
 
 }
